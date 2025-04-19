f9585b5cec3894ce9fdc5982e5698a9bba9557dd
Nigeriboy <abdurahmonabdullah247@gmail.com>
2025-04-19 21:15:38 +0100

    CREATE 4 FUNCTIONS
    I created (getHumanChoice, firstLetterUpperCase, playRound and playGame) functions
    to get the function up and running.

--- index.js
+++ index.js
@@ -7,4 +7,62 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let userInput = prompt("Enter your choice: 'Rock', 'Scissors', 'Paper'").toLowerCase();
+    return firstLetterUpperCase(userInput);
+}
+
+function firstLetterUpperCase(userInput){
+    let upperCased = userInput[0].toUpperCase();
+    let stringConcat = upperCased + userInput.slice(1);
+    return stringConcat;
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+
+
+function playRound(computerChoice, humanChoice){
+    if (computerChoice === humanChoice){
+        console.log(`It is a draw, you both chose ${computerChoice}.`);
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Scissors"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+        console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+    }
+    else if (humanChoice === "Scissors" && computerChoice === "Paper"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+        console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Rock"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+        console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        computerScore++;
+        console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`);
+    }
+}
+
+function playGame(){
+    const gameRound = 5;
+    for (let i = 1; i <= gameRound; i++){
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        playRound(computerChoice, humanChoice);
+    }
+
+    if (humanScore > computerScore){
+        console.log("You won the Game! :).")
+    }
+    else {
+        console.log("You lose the Game! :(.")
+    }
+}
+
+playGame();
